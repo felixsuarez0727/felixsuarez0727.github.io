c613c0da907e6918f5c708675f543fa248c28acf
felixsuarez0727 <felixdavidsuarezbonilla@gmail.com>
2025-03-10 21:17:06 +0000

    Deploying to gh-pages from @ felixsuarez0727/felixsuarez0727.github.io@75d6d365c219d391f869ae2c372a9c5b1793acdb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,14 +420,14 @@ ninja.data = [{
           description: "API designed to store and categorize videos using Google Cloud Video Intelligence.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-bioinformatics-pipeline",
+          title: 'Bioinformatics Pipeline',
+          description: "Bioinformatics pipeline that automates the processing of large biological datasets.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-challenger-dev-python",
+          title: 'challenger-dev-python',
+          description: "Traffic offenses system with an admin interface and API for report generation.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
